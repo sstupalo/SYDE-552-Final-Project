503a84cb86f2a6c13736f152706bec82537f315d
Sophie Stupalo <55926599+sstupalo@users.noreply.github.com>
2024-04-17 12:22:45 -0400

    Set rate equations to be solved each timestep

--- HH-neuron.py
+++ HH-neuron.py
@@ -8,7 +8,7 @@ start_scope()
 
 # PARAMETERS
 Cm        = 1*uF/cm**2          # Membrane capacitance
-Rl        = 100*ohm*cm          # longitudinal intracellular resistivity
+Rl        = 100*ohm*cm          # Longitudinal intracellular resistivity
 gNa       = 100*mS/meter**2     # Conductance of persistent sodium current
 gK        = 40*mS/meter**2      # Conductance of potassium current
 gAHP      = 0.01*mS/meter**2    # Conductance of afterhyperpolarization current
@@ -26,9 +26,9 @@ epsilon   = 1.2*Hz              # Diffusion constant
 k_o_inf   = 4.0*mM              # Steady state extracellular potassium concentration
 Cl_i      = 6.0*mM              # Intracellular chloride concentration
 Cl_o      = 130.0*mM            # Extracellular chloride concentration
-Na_i_rest = 18.0*mM             # Normal resting intracellular sodium concentration
-K_i_rest  = 140.0*mM            # Normal resting intracellular potassium concentration
-Na_o_rest = 144.0*mM             # Normal resting extracellular sodium concentration
+# Na_i_rest = 18.0*mM             # Normal resting intracellular sodium concentration
+# K_i_rest  = 140.0*mM            # Normal resting intracellular potassium concentration
+# Na_o_rest = 144.0*mM             # Normal resting extracellular sodium concentration
 
 # DYNAMIC VARIABLES
 # V     = 0 * mV             # Membrane potential (millivolts)
@@ -50,31 +50,30 @@ ICl = -gClL(v-VCl) : amp/meter**2
 
 dn/dt = rho*(alpha_n*(1-n) - beta_n*n) : Hz
 dh/dt = rho*(alpha_h*(1-h) - beta_h*h) : Hz
-dCa_i/dt = -0.002*gCa*(v-VCa)/(1+exp(-(v+25)/2.5))-(Ca_i/80) : mmolar/meter**3
+dCa_i/dt = -0.002*gCa*(v-VCa)/(1+exp(-(v+25)/2.5))-(Ca_i/80) : amp (point current)
 
 
-dK_o/dt = -0.33*IK - 2*beta*Ipump - Iglia - Idiff : mmolar/meter**3
+dK_o/dt = -0.33*IK - 2*beta*Ipump - Iglia - Idiff : amp (point current)
 Ipump = (rho/(1+exp((25-Na_i)/3)))*(1/(1+exp(5.5-K_o))) : amp/meter**2
 Iglia = Gglia/(1+exp((18-K_o)/2.5)) : amp/meter**2
 Idiff = epsilon*(K_o-k_o_inf) : amp/meter**2
 
-K_i = K_i_rest+(Na_i_rest-Na_i) : mmolar/meter**3
-dNa_i/dt = 0.33*INa/beta-3*Ipump : mmolar/meter**3
-Na_o = Na_o_rest-beta*(Na_i-Na_i_rest) : mmolar/meter**3
+K_i = 140.0+(18.0-Na_i) : mmolar/meter**3
+dNa_i/dt = 0.33*INa/beta-3*Ipump : amp (point current)
+Na_o = 144.0-beta*(Na_i-18.0) : mmolar/meter**3
 
 VNa = 26.64*log(Na_o/Na_i) : volt
 VK = 26.64*log(K_o/K_i) : volt
 VCl = 26.64*log(Cl_i/Cl_o) : volt
 
-m_inf = alpha_m/(alpha_m+beta_m) : 1
-alpha_m = 0.1*(v+30)/(1-exp(-0.1*(v+30))) : Hz
-beta_m = 4*exp(-(v+55)/18) : Hz
-alpha_n = 0.01*(v+34)/(1-exp(-0.1*(v+34))) : Hz
-beta_n = 0.125*exp(-(v+44)/80) : Hz
-alpha_h = 0.07*exp(-(v+44)/20) : Hz
-beta_h = 1/(1+exp(-0.1*(v+4))) : Hz
+m_inf = alpha_m/(alpha_m+beta_m) : 1 (constant over dt)
+alpha_m = 0.1*(v+30)/(1-exp(-0.1*(v+30))) : Hz (constant over dt)
+beta_m = 4*exp(-(v+55)/18) : Hz (constant over dt)
+alpha_n = 0.01*(v+34)/(1-exp(-0.1*(v+34))) : Hz (constant over dt)
+beta_n = 0.125*exp(-(v+44)/80) : Hz (constant over dt)
+alpha_h = 0.07*exp(-(v+44)/20) : Hz (constant over dt)
+beta_h = 1/(1+exp(-0.1*(v+4))) : Hz (constant over dt)
 '''
-
 
 soma = Soma(diameter=30*um)
 soma.axon = Cylinder(length=100*um, diameter=1*um, n=10)
