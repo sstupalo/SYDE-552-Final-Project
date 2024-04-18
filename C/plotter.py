28941382092f41b6a75652e50f1a7d64e2257b5c
Adam Courchesne <courchesneadam@gmail.com>
2024-04-18 16:31:42 -0400

    Updated plotter

--- C/plotter.py
+++ C/plotter.py
@@ -10,14 +10,32 @@ voltage = data[:, 1]
 potassium_concentration = data[:, 2]
 sodium_concentration = data[:, 3]
 
-# Plot the data
+# Plot the voltage data
 plt.figure(figsize=(10, 6))
-plt.plot(time, voltage, label='Voltage')
-plt.plot(time, potassium_concentration, label='Potassium Concentration')
-plt.plot(time, sodium_concentration, label='Sodium Concentration')
+plt.plot(time, voltage, label='Voltage', color='blue')
 plt.xlabel('Time (s)')
-plt.ylabel('Values')
-plt.title('Neuron Data')
+plt.ylabel('Voltage')
+plt.title('Voltage vs Time')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Plot the potassium concentration data
+plt.figure(figsize=(10, 6))
+plt.plot(time, potassium_concentration, label='Potassium Concentration', color='green')
+plt.xlabel('Time (s)')
+plt.ylabel('Potassium Concentration')
+plt.title('Potassium Concentration vs Time')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Plot the sodium concentration data
+plt.figure(figsize=(10, 6))
+plt.plot(time, sodium_concentration, label='Sodium Concentration', color='red')
+plt.xlabel('Time (s)')
+plt.ylabel('Sodium Concentration')
+plt.title('Sodium Concentration vs Time')
 plt.legend()
 plt.grid(True)
 plt.show()
